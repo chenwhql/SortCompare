5b60ea8630acad4ca2707e79cf1e75c52fd07ffe
Chen Weihang <sunny_cwh@163.com>
2018-10-17 23:17:46 +0800

    add images

--- mt19937ar.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MT_19937_AR_H_
-#define _MT_19937_AR_H_
-
-void init_genrand(unsigned long s);
-unsigned long genrand_int32(void);
-
-#endif
---
+++ src/mt19937ar.h
@@ -0,0 +1,7 @@
+#ifndef _MT_19937_AR_H_
+#define _MT_19937_AR_H_
+
+void init_genrand(unsigned long s);
+unsigned long genrand_int32(void);
+
+#endif
